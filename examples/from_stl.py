2dfd8ff9059f59438e98bc3647a014846d40da4c
tanmay <tpb2k99@gmail.com>
2021-09-23 14:07:41 +0530

    Made package adhere to setuptools template, added setup.py

--- examples/from_stl.py
+++ examples/from_stl.py
@@ -1,11 +1,11 @@
 # hack to import ros_acoustics module
-import pathlib
-import sys
-parent_dir = pathlib.Path(sys.argv[0]).\
-				parent.absolute().\
-				parent.absolute().\
-				__str__()
-sys.path.append(parent_dir)
+# import pathlib
+# import sys
+# parent_dir = pathlib.Path(sys.argv[0]).\
+# 				parent.absolute().\
+# 				parent.absolute().\
+# 				__str__()
+# sys.path.append(parent_dir)
 
 from pra_utils.core import ComplexRoom
 import matplotlib.pyplot as plt
