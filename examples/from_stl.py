841440d088c7e88c9a47359364333242a5db655b
enceladus2000 <tpb2k99@gmail.com>
2021-10-28 07:05:25 +0530

    added source and mic in from_stl example

--- examples/from_stl.py
+++ examples/from_stl.py
@@ -11,8 +11,13 @@ os.chdir(parent_dir)
 path_to_stl = 'data/mesh/simple_pipe.stl'
 
 room_material = pra.Material(0.5, None)
-room = ComplexRoom.from_stl(path_to_stl, room_material, reverse_normals=True, 
+room = ComplexRoom.from_stl(path_to_stl, room_material, reverse_normals=False, 
 		scale_factor=3., fs=15500, max_order=3,)
+
+room.add_source([1, 2.5, 2.5])
+room.add_microphone([3, 2.5, 2.5])
+room.compute_rir()
+room.plot_rir()
 
 print(room.fs)
 room.plot(show_normals={'length':.6})
